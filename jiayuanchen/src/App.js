d6f8feb59554ee69f467f92a8eed502a15c83ce0
geraint <geraintcjy@gmail.com>
2021-03-31 15:30:57 +0800

    typo

--- jiayuanchen/src/App.js
+++ jiayuanchen/src/App.js
@@ -6,7 +6,7 @@ export const App = () => {
     return (
         <div className="App" style = {{color: 'black'}}>
             <h1>帅哥的主页</h1>
-            <h2>我知道很丑。毕业有点搞，晚点再做</h2>
+            <h2>毕设有点搞，晚点再弄</h2>
         </div>
     );
 }
