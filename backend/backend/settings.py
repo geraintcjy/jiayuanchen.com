d52c562df0ca6bc86c46b5051b180ab78320b9aa
geraint <geraintcjy@gmail.com>
2021-04-06 17:18:54 +0800

    little

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '+yc_oxixzbfd#dabnce#x)ieckhsz1ock#!a8(!c(lhnuvf&n-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['1.15.77.57', 'jiayuanchen.com', 'localhost']
 
 # Application definition
 
--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -7,7 +7,7 @@ export default function Home (){
         <div className= 'Home'
              style = {{textAlign: 'center', fontFamily: 'Hiragino Sans GB', overflowX: 'hidden', backgroundColor: '#000034', height: '100vh'}}>
             <div className='text' style = {{color: '#F2F2F2'}}>
-                <MainTitle style = {{marginTop: '20vh'}}>帅哥的主页</MainTitle>
+                <MainTitle style = {{marginTop: '15vh'}}>帅哥的主页</MainTitle>
                 <MainTitle style = {{marginTop: '5vh', fontFamily: 'Arial, serif'}}>A Handsome Guy's Homepage</MainTitle>
                 <form method = 'POST' action = "http://127.0.0.1:8000/postMessage/" style = {{marginTop: '10vh', textAlign: 'center'}}>
                     <DjangoCSRFToken />
@@ -18,7 +18,6 @@ export default function Home (){
                     <Submit type = 'submit' value = 'Send' />
                 </form>
                 <div style = {{fontSize: '15px', marginTop: '15vh'}}>现在搞毕设 之后再弄</div>
-                <div style = {{fontSize: '15px', marginTop: '1vh'}}>PS：别点</div>
             </div>
         </div>
     )
@@ -34,19 +33,22 @@ const InputBox = styled.input`
   border: none;
   color: #F2F2F2;
   text-align: center;
+  font-family: Arial,serif;
 `
 const Title = styled.div`
   font-size: 20px;
   color: #CC5500;
   font-weight: bold;
   margin-top: 2vh;
+  font-family: Arial,serif;
   -webkit-user-select: none;
   -moz-user-select:none;
   -ms-user-select:none;
 `
 const Submit = styled.input`
+  -webkit-appearance:none;
   background-color: #CC5500;
-  border: #CC5500 2px solid;
+  border: none;
   font-size: 20px;
   margin: 4vh auto 0 auto;
   height: 50px;
@@ -54,6 +56,7 @@ const Submit = styled.input`
   color: #FFFFFF;
   border-radius: 10px;
   cursor: pointer;
+  font-family: Arial,serif;
 `
 
 const MainTitle = styled.div`
