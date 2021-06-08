d1c3b87f0713a4db0521b36755d0b193b10254b4
geraint <geraintcjy@gmail.com>
2021-06-08 18:36:43 +0800

    three.js need to be added

---
+++ frontend/src/Config.js
@@ -0,0 +1,3 @@
+const BACKEND_DOMAIN = "https://jiayuanchen.com:8002";
+
+export { BACKEND_DOMAIN };
--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -2,15 +2,18 @@ import React, {useEffect} from 'react';
 import DjangoCSRFToken from "django-react-csrftoken/src";
 import styled from 'styled-components';
 import axios from 'axios';
-import qs from 'qs'
+import qs from 'qs';
+import * as THREE from 'three';
+import * as config from '../Config';
 
 export default function Home (){
     useEffect(()=>{
-        axios.post("https://jiayuanchen.com:8002/initialization/", {})
+        axios.post(config.BACKEND_DOMAIN + "/initialization/", {})
             .then(res => {})
             .catch(error => {})
-
         console.log(navigator.userAgent);
+
+
     })
 
     return(
@@ -18,38 +21,40 @@ export default function Home (){
              style = {{textAlign: 'center', fontFamily: 'Hiragino Sans GB', overflowX: 'hidden', backgroundColor: '#000034', height: '100vh'}}>
             <div className='text' style = {{color: '#F2F2F2', display: 'flex', flexDirection: 'column'}}>
                 <MainTitle isPC = {isPC()} style = {{marginTop: '5vh'}}>
-                    Jiayuan Chen | 陈嘉缘
+                    Jiayuan Chen (陈嘉缘)
                 </MainTitle>
                 <Text isPC = {isPC()} style = {{marginTop: '5vh'}}>
                     Hi, my name is Jiayuan Chen, a senior studying Civil Engineering at Tongji University.
                 </Text>
                 <Text isPC = {isPC()}>
-                    I see myself as a novelty seeker, a team player and an eclecticist. I love tech all the time, parties in most circumstances, and soccer occasionally.
+                    I see myself as a novelty seeker, a team player and an eclecticist. I love stuff with innovation and passion, such as tech, parties and soccer.
                 </Text>
                 <Text isPC = {isPC()}>
                     In this fall, I am going to Georgia Tech to pursue a Master's degree in Computational Science and Engineering.
-                    Should you have any requests or questions, please feel free to contact me.
+                    Should you have any requests, please feel free to contact me.
                 </Text>
-                <form method = 'POST' action = "https://jiayuanchen.com:8002/postMessage/" style = {{marginTop: '8vh', textAlign: 'center'}}>
+
+                <form method = 'POST' action = {config.BACKEND_DOMAIN + "/postMessage/"} style = {{marginTop: '8vh', textAlign: 'center'}} onSubmit={messageBoardCheck}>
                     <DjangoCSRFToken />
                     <Title>Below is a message board</Title>
-                    <InputBox name = 'content' style = {{width: '90vw'}}/>
+                    <InputBox name = 'content' id = 'content' style = {{width: '90vw'}}/>
                     <div style = {{width: '60vw', height: '2px', backgroundColor: '#CC5500', margin: 'auto', marginBottom: '5vh'}}/>
                     <Title>Signature</Title>
-                    <InputBox name = 'name' style = {{width: '200px'}}/>
+                    <InputBox name = 'name' id = 'name' style = {{width: '200px'}}/>
                     <div style = {{width: '220px', height: '2px', backgroundColor: '#CC5500', margin: 'auto'}}/>
                     <Submit type = 'submit' value = 'Send' />
                 </form>
+
                 <div style = {{marginTop: '6vh', fontFamily: 'Verdana, sans-serif'}}>
                     Wechat: geraintcjy
-                    <br />
-                    <br />
+                </div>
+                <div style = {{marginTop: '1vh', fontFamily: 'Verdana, sans-serif'}}>
+                    Email: geraintcjy@gmail.com
+                </div>
+                <div style = {{marginTop: '1vh', fontFamily: 'Verdana, sans-serif'}}>
                     <a
                         href = "https://beian.miit.gov.cn/"
-                        style = {{
-                            textDecoration: 'none',
-                            color: '#F2F2F2'
-                        }}>沪ICP备2021009744号</a>
+                        style = {{textDecoration: 'none', color: '#F2F2F2'}}>沪ICP备2021009744号</a>
                 </div>
             </div>
         </div>
@@ -70,6 +75,13 @@ function isPC() {
         flag = true;
     }
     return flag;
+}
+function messageBoardCheck(e){
+    const contentBox = document.getElementById("content");
+    if (contentBox.value === ""){
+        alert("不要灌水");
+        e.preventDefault();
+    }
 }
 
 const InputBox = styled.input`
@@ -106,6 +118,10 @@ const Submit = styled.input`
   border-radius: 10px;
   cursor: pointer;
   font-family: Verdana, sans-serif;
+  &:hover{
+    background-color: #B54D00;
+  }
+  
 `
 const MainTitle = styled.div`
   font-size: ${props => {return props.isPC ? '35px' : '25px'}};
