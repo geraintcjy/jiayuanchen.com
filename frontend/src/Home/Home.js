95691e675f8ef5367de50ac206c07d12205162db
geraint <geraintcjy@gmail.com>
2021-06-15 19:07:30 +0800

    Add Three.js Background

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -13,111 +13,7 @@ export default function Home (){
             .then(res => {})
             .catch(error => {})
 
-        let scene, camera, renderer, cloudParticles = [], flash, rain, rainGeo, rainCount = 8000;
-
-        function init() {
-            scene = new THREE.Scene();
-            camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
-            camera.position.z = 1;
-            camera.rotation.x = 1.16;
-            camera.rotation.y = -0.12;
-            camera.rotation.z = 0.27;
-            const ambient = new THREE.AmbientLight(0x555555);
-            scene.add(ambient);
-            const directionalLight = new THREE.DirectionalLight(0xffeedd);
-            directionalLight.position.set(0,0,1);
-            scene.add(directionalLight);
-            flash = new THREE.PointLight(0x062d89, 30, 500 ,4.5);
-            flash.position.set(200,300,100);
-            scene.add(flash);
-            renderer = new THREE.WebGLRenderer();
-            scene.fog = new THREE.FogExp2(0x11111f, 0.002);
-            renderer.setClearColor(scene.fog.color);
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            document.getElementById('home-background').appendChild(renderer.domElement);
-
-            let drops = [];
-            for(let i=0; i<rainCount; i++) {
-                drops.push(Math.random() * 400 - 200); //x
-                drops.push(Math.random() * 1000 - 500); //y
-                drops.push(Math.random() * 400 - 200); //z
-                drops.push(0); //velocity
-            }
-            drops = new Float32Array(drops);
-            rainGeo = new THREE.BufferGeometry();
-            rainGeo.setAttribute( 'position', new THREE.BufferAttribute(drops, 4));
-
-            const rainMaterial = new THREE.PointsMaterial({
-                color: 0xaaaaaa,
-                size: 0.1,
-                transparent: true
-            });
-            rain = new THREE.Points(rainGeo, rainMaterial);
-            scene.add(rain);
-
-            domready(function(){
-                const loader = new THREE.TextureLoader();
-                loader.load('./Home/smoke-1.png', (texture) => {
-                    let cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
-                    const cloudMaterial = new THREE.MeshLambertMaterial({
-                        map: texture,
-                        transparent: true,
-                    })
-                    for(let p=0; p<25; p++) {
-                        let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
-                        cloud.position.set(
-                            Math.random()*1100 - 550,
-                            500,
-                            Math.random()*800 - 400
-                        );
-                        cloud.rotation.x = 1.16;
-                        cloud.rotation.y = -0.12;
-                        cloud.rotation.z = Math.random()*360;
-                        cloud.material.opacity = 0.6;
-                        cloudParticles.push(cloud);
-                        scene.add(cloud);
-                    }
-                    animate();
-                });
-            })
-        }
-
-        function animate() {
-            cloudParticles.forEach(p => {
-                p.rotation.z -= 0.002;
-            });
-            for (let i=0; i<rainGeo.attributes.position.count; i++){
-                rainGeo.attributes.position.array[4*i+3] -= 0.1 + Math.random() * 0.1;
-                rainGeo.attributes.position.array[4*i+1] += rainGeo.attributes.position.array[4*i+3];
-                if (rainGeo.attributes.position.array[4*i+1] < -500){
-                    rainGeo.attributes.position.array[4*i+1] = 500;
-                    rainGeo.attributes.position.array[4*i+3] = 0;
-                }
-            }
-            // rainGeo.vertices.forEach(p => {
-            //     p.velocity -= 0.1 + Math.random() * 0.1;
-            //     p.y += p.velocity;
-            //     if (p.y < -200) {
-            //         p.y = 200;
-            //         p.velocity = 0;
-            //     }
-            // });
-            rainGeo.attributes.position.needsUpdate = true;
-            rain.rotation.y += 0.002;
-            if(Math.random() > 0.98 || flash.power > 100) {
-                if(flash.power < 100)
-                    flash.position.set(
-                        Math.random()*400,
-                        300 + Math.random() *200,
-                        100
-                    );
-                flash.power = 50 + Math.random() * 500;
-            }
-            renderer.render(scene, camera);
-            requestAnimationFrame(animate);
-        }
-
-        init();
+    setRainBack();
     })
 
     return(
@@ -170,6 +66,113 @@ export default function Home (){
     )
 }
 
+export function setRainBack(){
+    let scene, camera, renderer, cloudParticles = [], flash, rain, rainGeo, rainCount = 8000;
+
+    function init() {
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
+        camera.position.z = 1;
+        camera.rotation.x = 1.16;
+        camera.rotation.y = -0.12;
+        camera.rotation.z = 0.27;
+        const ambient = new THREE.AmbientLight(0x555555);
+        scene.add(ambient);
+        const directionalLight = new THREE.DirectionalLight(0xffeedd);
+        directionalLight.position.set(0,0,1);
+        scene.add(directionalLight);
+        flash = new THREE.PointLight(0x062d89, 30, 500 ,4.5);
+        flash.position.set(200,300,100);
+        scene.add(flash);
+        renderer = new THREE.WebGLRenderer();
+        scene.fog = new THREE.FogExp2(0x11111f, 0.002);
+        renderer.setClearColor(scene.fog.color);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('home-background').appendChild(renderer.domElement);
+
+        let drops = [];
+        for(let i=0; i<rainCount; i++) {
+            drops.push(Math.random() * 400 - 200); //x
+            drops.push(Math.random() * 1000 - 500); //y
+            drops.push(Math.random() * 400 - 200); //z
+            drops.push(0); //velocity
+        }
+        drops = new Float32Array(drops);
+        rainGeo = new THREE.BufferGeometry();
+        rainGeo.setAttribute( 'position', new THREE.BufferAttribute(drops, 4));
+
+        const rainMaterial = new THREE.PointsMaterial({
+            color: 0xaaaaaa,
+            size: 0.1,
+            transparent: true
+        });
+        rain = new THREE.Points(rainGeo, rainMaterial);
+        scene.add(rain);
+
+        domready(function(){
+            const loader = new THREE.TextureLoader();
+            loader.load('./Home/smoke-1.png', (texture) => {
+                let cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+                const cloudMaterial = new THREE.MeshLambertMaterial({
+                    map: texture,
+                    transparent: true,
+                })
+                for(let p=0; p<25; p++) {
+                    let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+                    cloud.position.set(
+                        Math.random()*1100 - 550,
+                        500,
+                        Math.random()*800 - 400
+                    );
+                    cloud.rotation.x = 1.16;
+                    cloud.rotation.y = -0.12;
+                    cloud.rotation.z = Math.random()*360;
+                    cloud.material.opacity = 0.6;
+                    cloudParticles.push(cloud);
+                    scene.add(cloud);
+                }
+                animate();
+            });
+        })
+    }
+
+    function animate() {
+        cloudParticles.forEach(p => {
+            p.rotation.z -= 0.002;
+        });
+        for (let i=0; i<rainGeo.attributes.position.count; i++){
+            rainGeo.attributes.position.array[4*i+3] -= 0.1 + Math.random() * 0.1;
+            rainGeo.attributes.position.array[4*i+1] += rainGeo.attributes.position.array[4*i+3];
+            if (rainGeo.attributes.position.array[4*i+1] < -500){
+                rainGeo.attributes.position.array[4*i+1] = 500;
+                rainGeo.attributes.position.array[4*i+3] = 0;
+            }
+        }
+        // rainGeo.vertices.forEach(p => {
+        //     p.velocity -= 0.1 + Math.random() * 0.1;
+        //     p.y += p.velocity;
+        //     if (p.y < -200) {
+        //         p.y = 200;
+        //         p.velocity = 0;
+        //     }
+        // });
+        rainGeo.attributes.position.needsUpdate = true;
+        rain.rotation.y += 0.002;
+        if(Math.random() > 0.98 || flash.power > 100) {
+            if(flash.power < 100)
+                flash.position.set(
+                    Math.random()*400,
+                    300 + Math.random() *200,
+                    100
+                );
+            flash.power = 50 + Math.random() * 500;
+        }
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+
+    init();
+}
 function isPC() {
     const userAgentInfo = navigator.userAgent;
     const Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPod"];
--- frontend/src/Submitted/Submitted.js
+++ frontend/src/Submitted/Submitted.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom'
+import { setRainBack } from "../Home/Home";
 
 export default function Submitted (){
+    useEffect(()=>{
+        setRainBack();
+    })
+
+
     return(
         <div className= 'Submitted'
-             style = {{textAlign: 'center', fontFamily: 'Hiragino Sans GB', overflowX: 'hidden', backgroundColor: '#000034', height: '100vh'}}>
+             style = {{textAlign: 'center', fontFamily: 'Hiragino Sans GB', overflowX: 'hidden', backgroundColor: 'transparent', height: '100vh'}}>
             <div className='text' style = {{color: '#F2F2F2'}}>
                 <MainTitle style = {{marginTop: '40vh'}}>
                     Thanks
                 </MainTitle>
                 <div style = {{marginTop: '5vh'}}><Link to="/home" style = {{color: '#F2F2F2'}}>返回</Link></div>
+            </div>
+            <div id = "home-background" style = {{position: 'absolute', zIndex: '-1', top: '0'}}>
+
             </div>
         </div>
     )
