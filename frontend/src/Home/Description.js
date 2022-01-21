f7ba7bf3e6dc10eabd9dd861b970d5dc01916d5d
geraint <geraintcjy@gmail.com>
2022-01-20 21:13:42 -0600

    0120

--- frontend/src/Home/Description.js
+++ frontend/src/Home/Description.js
@@ -6,29 +6,29 @@ export default function Description() {
     return (
         <div>
             <Text isPC = { isPC() }>&emsp;&emsp;
-                <span>Hi there! Welcome to my website and thanks for visiting!</span>
+                <span>Hi there! Welcome to my website and thanks for visiting! </span>
                 <span>My name is Jiayuan (jah-yoo-aan) Chen, and I am a CSE Master's student at Georgia Tech.</span>
             </Text>
             <Text isPC = { isPC() }>&emsp;&emsp;
                 <span>I see myself as a novelty seeker, a hard worker as well as a perfect team player. </span>
-                <span>The projects I have worked on in school were mainly about Digitalization and Machine Learning in Civil Engineering.</span>
-                <span>Through my last two years in undergraduate I participated in a couple of research programs and competitions.</span>
-                <span>which were quite interesting had potential applications in future structures' development and maintenance.</span>
+                <span>The projects I have worked on in school were mainly about Digitalization and Machine Learning in Civil Engineering. </span>
+                <span>Through my last two years in undergraduate I participated in a couple of research programs and competitions, </span>
+                <span>which were quite interesting could have potential applications in future structures' development and maintenance. </span>
             </Text>
             <Text isPC = { isPC() }>&emsp;&emsp;
-                <span>I am good at a variety of games and activities. </span>
-                <span>Among all, basketball, soccer, and board games are my favorite. I am also a big fan of Paradox games and I wasted a lot of time on them, which was not good.</span>
-                <span>The photo on the right was shot by my GF during our trip to SLC this winter. It was really a wonderful trip. Definitely would visit again.</span>
+                <span>I play a variety of games and activities. </span>
+                <span>Among all, basketball, soccer, and board games are my favorite. I am also a big fan of Paradox games and I wasted a lot of time on them, which was not very good. </span>
+                <span>The photo on the right was shot by my GF during our trip to SLC this winter. It was such a wonderful trip. Definitely would visit again. </span>
             </Text>
             <Text isPC = { isPC() }>&emsp;&emsp;
-                <span>My graduate date can be either in Dec, 2022 or in May, 2023, depending on whether I need to conduct a Co-op internship or not.</span>
+                <span>My graduate date can be either in Dec, 2022 or in May, 2023, depending on if I need to conduct a Co-op internship or not. </span>
                 <span>I am currently looking for 2022 summer internship as well as Fall 2022 Co-op internship opportunities. </span>
                 <span>Please feel free to contact me about anything :) </span>
             </Text>
 
             <Text isPC = { isPC() }>&emsp;&emsp;
                     <span>
-                        You can download my resume through this
+                        BTW, You could download my resume through this
                     </span>
                     {' '}
                     <span>
